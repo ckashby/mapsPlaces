0e4abd13692371eaeedbd9ffac5a313d94a5964e
Clay Ashby <clay.ashby@gmail.com>
2022-10-20 10:34:47 -0400

    init

---
+++ src/SharePlace.js
@@ -0,0 +1,40 @@
+export class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    addressForm.addEventListener('submit', this.findAddressHandler);
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+  }
+
+  locateUserHandler = () => {
+    if (!navigator.geolocation) {
+      alert(
+        'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+      );
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          'Could not locate you unfortunately. Please enter an address manually!'
+        );
+      }
+    );
+  };
+
+  findAddressHandler = (event) => {
+    event.preventDefault();
+    const address = document.getElementById('address').value;
+    console.log(address);
+  };
+}
+
+new PlaceFinder();
